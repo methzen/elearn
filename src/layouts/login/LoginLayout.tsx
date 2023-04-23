dc36a79e24bef15acfe81ac320f4534936dae081
mouhamadou dia <mouhamadou.dia@medadom.com>
2023-04-23 13:08:34 +0200

    clean

--- src/layouts/login/LoginLayout.tsx
+++ src/layouts/login/LoginLayout.tsx
@@ -25,23 +25,6 @@ export default function LoginLayout({ children, illustration, title }: Props) {
           ml: { xs: 2, md: 5 },
         }}
       />
-
-      <StyledSection>
-        <Typography variant="h3" sx={{ mb: 10, maxWidth: 480, textAlign: 'center' }}>
-          {title || 'Hi, Welcome back'}
-        </Typography>
-
-        <Image
-          disabledEffect
-          visibleByDefault
-          alt="auth"
-          src={illustration || '/assets/illustrations/illustration_dashboard.png'}
-          sx={{ maxWidth: 720 }}
-        />
-
-        <StyledSectionBg />
-      </StyledSection>
-
       <StyledContent>
         <Stack sx={{ width: 1 }}> {children} </Stack>
       </StyledContent>
--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -3,13 +3,15 @@
 function path(root: string, sublink: string) {
   return `${root}${sublink}`;
 }
-
+const ROOTS_AUTH = '/auth';
 const ROOTS_DASHBOARD = '/dashboard';
 
 // ----------------------------------------------------------------------
 
 export const PATH_AUTH = {
-  login: '/login',
+  root: ROOTS_AUTH,
+  login: path(ROOTS_AUTH, '/login'),
+  register: path(ROOTS_AUTH, '/register'),
 };
 
 export const PATH_DASHBOARD = {
